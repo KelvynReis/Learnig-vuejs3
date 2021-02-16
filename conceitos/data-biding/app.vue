ffe5fd40c147e2dc5e4aa689f3ab8555d790db8b
kelvyn <kelvyn2reis@gmail.com>
2021-02-16 14:13:54 -0300

    init

---
+++ conceitos/data-biding/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 :style="{ textDecoration: decoration }">Hello {{ name }}!</h1>
+    <br>
+    <input type="text" v-model="name">
+    <br>
+    <a :href="link">Link pro curso!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Rudys',
+    link: 'https://treinamento.vuejsbrasil.org',
+    decoration: 'underline'
+  })
+}
+</script>
+
+<style>
+    div{
+        margin: 0;
+        padding: 0;
+        width: 100%;
+
+        display: flex;
+        justify-content: center;
+        align-content: center;
+    }
+    
+</style>
